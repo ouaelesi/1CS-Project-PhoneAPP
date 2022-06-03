4c552999ae855dd0b7da9be2dbc5c11e7d614790
KamelMohammedYamani <jm_yamani@esi.dz>
2022-06-03 20:09:22 +0100

    Adding PointIntPage

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
-import { StyleSheet } from "react-native";
-import { NavigationContainer } from "@react-navigation/native";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import {StyleSheet} from "react-native";
+import {NavigationContainer} from "@react-navigation/native";
+import {createNativeStackNavigator} from "@react-navigation/native-stack";
 import HomePage from "./Pages/HomePage";
 import MapPage from "./Pages/MapPage";
 import PointIntPage from "./Pages/PointIntPage"
@@ -8,24 +8,25 @@ import PointIntPage from "./Pages/PointIntPage"
 const Stack = createNativeStackNavigator();
 
 export default function App() {
-  return (
-    <NavigationContainer>
-      <Stack.Navigator
-        screenOptions={{
-          headerShown: false,
-        }}
-      >
-        <Stack.Screen name="PointInt" component={PointIntPage} />
+    return (
+        <NavigationContainer>
+            <Stack.Navigator
+                screenOptions={{
+                headerShown: false
+            }}>
+                <Stack.Screen name="HomePage" component={HomePage}/>
+                <Stack.Screen name="MapPage" component={MapPage}/>
+                <Stack.Screen name="PointInt" component={PointIntPage}/>
 
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
+            </Stack.Navigator>
+        </NavigationContainer>
+    );
 }
 
 const styles = StyleSheet.create({
-  appContainer: {
-    paddingHorizontal: 10,
-    flex: 1,
-    position: "relative",
-  },
+    appContainer: {
+        paddingHorizontal: 10,
+        flex: 1,
+        position: "relative"
+    }
 });
