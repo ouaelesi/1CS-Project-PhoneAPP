2ceaafafaf2e3e3c2bc651d354eb3bb90a2e8bdb
moradlarbi <moradlarbi1@gmail.com>
2022-06-08 22:18:56 +0100

    auth pages

--- App.js
+++ App.js
@@ -6,7 +6,8 @@ import MapPage from "./Pages/MapPage";
 import ZonePage from "./Pages/ZonePage";
 import ExplorePage from "./Pages/ExplorePage";
 import PointIntPage from "./Pages/PointIntPage";
-
+import LoginScreen from "./Pages/LoginScreen";
+import RegisterScreen from "./Pages/RegisterScreen";
 
 const Stack = createNativeStackNavigator();
 
@@ -17,9 +18,12 @@ export default function App() {
     return (
         <NavigationContainer>
             <Stack.Navigator
+                initialRouteName="LoginPage"
                 screenOptions={{
                 headerShown: false
             }}>
+                <Stack.Screen name="RegisterPage" component={RegisterScreen}/>
+                <Stack.Screen name="LoginPage" component={LoginScreen}/>
                 <Stack.Screen name="HomePage" component={HomePage}/>
                 <Stack.Screen name="MapPage" component={MapPage}/>
                 <Stack.Screen name="ZonePage" component={ZonePage} /> 
